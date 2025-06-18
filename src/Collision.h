9ad35f4e9614440015cc8a64a1c7482b85de1d72
ksodhi02 <ksodhi02@students.cs.ubc.ca>
2025-06-17 21:13:08 -0700

    Add collision detection functions for Circle and AABB shapes in Collision.h; introduce Contact struct for collision resolution details.

--- src/Collision.h
+++ src/Collision.h
@@ -1,4 +1,22 @@
 #pragma once
 #include "Body.h"
 
-void CheckAndResolveCollision(Body* a, Body* b);
+struct Contact {
+    Vec2 normal;
+    float penetration;
+    Vec2 contactPoint;
+};
+
+bool CheckCircleCircleCollision(const Circle* a, const Vec2& posA, 
+                              const Circle* b, const Vec2& posB,
+                              Contact& contact);
+
+bool CheckAABBAABBCollision(const AABB* a, const Vec2& posA,
+                           const AABB* b, const Vec2& posB,
+                           Contact& contact);
+
+bool CheckCircleAABBCollision(const Circle* circle, const Vec2& circlePos,
+                             const AABB* box, const Vec2& boxPos,
+                             Contact& contact);
+
+void ResolveCollision(Body* a, Body* b, const Contact& contact);
