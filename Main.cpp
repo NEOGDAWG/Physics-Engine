a9bc1620117f02bcc55c40d23b6af3a466910410
ksodhi02 <ksodhi02@students.cs.ubc.ca>
2025-06-18 23:22:28 -0700

    Remove binary from repo and update .gitignore

--- Main.cpp
+++ Main.cpp
@@ -5,28 +5,27 @@
 #include "src/World.h"
 #include <SFML/Graphics.hpp>
 
-int main() {
-    World world(Vec2(0, -9.8f)); // gravity down
-
-    // Create floor (AABB)
-    Body* floor = new Body(Vec2(0, -5), Vec2(10, 1), 1.0f, 0.6f, true);
-    
-    // Create a circle
-    Body* ball = new Body(Vec2(0, 5), 1.0f, 1.0f, 0.6f, false);
-    
-    // Create a box
-    Body* box = new Body(Vec2(2, 3), Vec2(1, 1), 1.0f, 0.6f, false);
-
-    world.AddBody(floor);
-    world.AddBody(ball);
-    world.AddBody(box);
-
-    for (int i = 0; i < 120; ++i) {
-        world.Update(0.016f); // 60 fps
-        std::cout << "t=" << i << ":\n"
-                  << "Ball pos = (" << ball->position.x << ", " << ball->position.y << ")\n"
-                  << "Box pos = (" << box->position.x << ", " << box->position.y << ")\n"
-                  << "-------------------\n";
+int main()
+{
+    unsigned int width = 650;
+    unsigned int height = 380;
+    sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({width, height}), "Physics Engine");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    while (window->isOpen()){
+        while(auto event = window->pollEvent()){
+            if(event->is<sf::Event::Closed>()){
+                window->close();
+            }
+            else if(const auto* keyEvent = event->getIf<sf::Event::KeyPressed>()){
+                if(keyEvent->scancode == sf::Keyboard::Scancode::Escape){
+                    window->close();
+                }
+            }
+        }
+        window->clear();
+        window->draw(shape);
+        window->display();
     }
 
     return 0;
